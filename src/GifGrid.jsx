6d720703a160571639971b617f4503111b8cda23
Kent <Kent@crimany-x>
2026-05-28 18:05:55 -0500

    updates to GifGrid component

--- src/GifGrid.jsx
+++ src/GifGrid.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
-function GifGrid() {
-
+function GifGrid({ gifs }) {
   return (
-    <>
-    </>
-  )
+    <div className="gif-grid">
+      {gifs.map((gif) => (
+        <div key={gif.id} className="gif-item">
+          <a href={gif.url} target="_blank" rel="noopener noreferrer">
+            {/* Use fixed_width_downsampled for better performance */}
+            <img 
+              src={gif.images.fixed_width_downsampled.url} 
+              alt={gif.title || "GIF"} 
+            />
+          </a>
+          <p>{gif.title || "Untitled GIF"}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default GifGrid;   
